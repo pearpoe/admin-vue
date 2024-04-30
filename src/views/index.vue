640acf76724edb20b9439a50f66924f60c336977
pine-storm <280722781@qq.com>
2024-04-30 12:52:54 +0800

    🎨 更新.gitignore文件以忽略一些文件和目录

---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-container home">
+    产品管理系统
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
+
